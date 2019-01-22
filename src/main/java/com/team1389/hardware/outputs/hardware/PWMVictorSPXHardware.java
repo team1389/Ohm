43009c948f23234a7386fea6b34b73011691158b
Quunii <kennethmiuraubw@gmail.com>
2019-01-22 12:33:34 -0500

    commented untested PWMVictorSPXHardware

--- src/main/java/com/team1389/hardware/outputs/hardware/PWMVictorSPXHardware.java
+++ src/main/java/com/team1389/hardware/outputs/hardware/PWMVictorSPXHardware.java
@@ -12,40 +12,65 @@ import com.team1389.watch.Watchable;
 
 import edu.wpi.first.wpilibj.PWMVictorSPX;
 
+/**
+ * provides voltage control stream for a PWM Victor SPX  
+ */
 public class PWMVictorSPXHardware extends Hardware<PWM>{
+    
+     
     private Optional<PWMVictorSPX> wpiVictor;
     private boolean outputInverted;
 
-    public PWMVictorSPXHardware(PWM port, Registry registry){
-        this(false, port, registry);
+    
+    public PWMVictorSPXHardware(PWM requestedPort, Registry registry){
+        this(false, requestedPort, registry);
     }
-    public PWMVictorSPXHardware( boolean outputInverted, PWM port,Registry registry){
+    /**
+     * @param outputInverted
+     *      Whether to reverse motor direction
+     * @param requestedPort
+     *      PWM port to initialize hardware on
+     * @param registry
+     *      registry for the client robot project
+     */
+    public PWMVictorSPXHardware(boolean outputInverted, PWM requestedPort,Registry registry){
         this.outputInverted = outputInverted;
-        attachHardware(port, registry);
+        attachHardware(requestedPort, registry);
     }
 
 	/**
-	 * @return a voltage output stream for this victor
+	 * @return a voltage output stream for this Victor SPX
 	 */
 	public PercentOut getVoltageController() {
 		return new PercentOut(voltage -> wpiVictor.ifPresent(s -> s.set(voltage)));
 	}
 
+    /**
+     * @return the list of watchables with a watchable for the voltage output stream added
+     */
 	@Override
 	public AddList<Watchable> getSubWatchables(AddList<Watchable> stem) {
 		return super.getSubWatchables(stem).put(getVoltageController().getWatchable("voltage"));
     }
 
+    /**
+     * runs if port is open. Configures Victor to match settings from constructors
+     */
     @Override
     protected void init(PWM port) {
         PWMVictorSPX victor = new PWMVictorSPX(port.index());
         victor.setInverted(outputInverted);
         wpiVictor = Optional.of(victor);
     }
+
+    /**
+     * runs if port is not open
+     */
     @Override
     protected void failInit() {
         wpiVictor = Optional.empty();
     }
+    
     @Override
     protected String getHardwareIdentifier() {
         return "PWM Victor SPX";

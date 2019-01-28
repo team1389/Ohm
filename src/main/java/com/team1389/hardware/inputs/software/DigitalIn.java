a3a736f63be8530a72f16b072a767765812a6e0f
Quunii <kennethmiuraubw@gmail.com>
2019-01-28 17:57:14 -0500

    DigitalIn ops don't affect calling instance

--- src/main/java/com/team1389/hardware/inputs/software/DigitalIn.java
+++ src/main/java/com/team1389/hardware/inputs/software/DigitalIn.java
@@ -11,51 +11,62 @@ import com.team1389.hardware.inputs.interfaces.ListeningBinaryInput;
 import com.team1389.watch.info.BooleanInfo;
 
 /**
- * a boolean input stream. The purpose of this class wrapped around {@link BinaryInput} is to make
- * the stream operations chainable (you can call them on a stream instance and they return the same
- * stream instance)
+ * a boolean input stream. The purpose of this class wrapped around
+ * {@link BinaryInput} is to make the stream operations chainable (you can call
+ * them on a stream instance and they return the same stream instance)
  * 
  * @author amind
  *
  */
-public class DigitalIn {
+public class DigitalIn
+{
 	private BinaryInput input;
 
 	/**
-	 * @param input the supplier of boolean values
+	 * @param input
+	 *                  the supplier of boolean values
 	 */
-	public DigitalIn(Supplier<Boolean> input) {
+	public DigitalIn(Supplier<Boolean> input)
+	{
 		this.input = input::get;
 	}
 
 	/**
 	 * 
-	 * @param in the supplier of boolean values
-	 * @param type the desired input filter
+	 * @param in
+	 *                 the supplier of boolean values
+	 * @param type
+	 *                 the desired input filter
 	 */
-	public DigitalIn(BinaryInput in, InputFilter type) {
+	public DigitalIn(BinaryInput in, InputFilter type)
+	{
 		this(in);
 		getSpecial(type);
 	}
 
 	/**
-	 * @param name the String identifier of the watchable
+	 * @param name
+	 *                 the String identifier of the watchable
 	 * @return a watchable that tracks the value of this stream
 	 */
-	public BooleanInfo getWatchable(String name) {
+	public BooleanInfo getWatchable(String name)
+	{
 		return new BooleanInfo(name, input::get);
 	}
 
 	/**
-	 * @param filterType the desired input filter
+	 * @param filterType
+	 *                       the desired input filter
 	 * @return the filtered stream
 	 */
-	public DigitalIn getSpecial(InputFilter filterType) {
-		switch (filterType) {
+	public DigitalIn getSpecial(InputFilter filterType)
+	{
+		switch (filterType)
+		{
 		case LATCHED:
-			return latched();
+			return getLatched();
 		case TOGGLED:
-			return toggled();
+			return getToggled();
 		default:
 			return this;
 		}
@@ -66,7 +77,8 @@ public class DigitalIn {
 	 * @see com.team1389.util.boolean_util.LatchedBoolean LatchedBoolean
 	 * @see com.team1389.util.boolean_util.ToggleBoolean ToggleBoolean
 	 */
-	public enum InputFilter {
+	public enum InputFilter
+	{
 		/**
 		 * 
 		 */
@@ -81,84 +93,98 @@ public class DigitalIn {
 	 * @return the latched version of this boolean stream
 	 * @see com.team1389.util.boolean_util.LatchedBoolean LatchedBoolean
 	 */
-	public DigitalIn latched() {
-		this.input = BinaryInput.getLatched(input);
-		return this;
+	public DigitalIn getLatched()
+	{
+		BinaryInput newInput = BinaryInput.getLatched(input);
+		return new DigitalIn(newInput);
 	}
 
 	/**
 	 * @return the toggled version of this boolean stream
 	 * @see com.team1389.util.boolean_util.ToggleBoolean ToggleBoolean
 	 */
-	public DigitalIn toggled() {
-		this.input = BinaryInput.getToggled(input);
-		return this;
+	public DigitalIn getToggled()
+	{
+		BinaryInput newInput = BinaryInput.getToggled(input);
+		return new DigitalIn(newInput);
 	}
 
 	/**
-	 * combines a list of boolean streams into a single stream that returns true if and only if all
-	 * of the original streams are true
+	 * combines a list of boolean streams into a single stream that returns true
+	 * if and only if all of the original streams are true
 	 * 
-	 * @param toCombine the list of streams to combine
+	 * @param toCombine
+	 *                      the list of streams to combine
 	 * @return the combined stream
 	 */
-	public DigitalIn combineAND(DigitalIn... toCombine) {
+	public DigitalIn getCombineAND(DigitalIn... toCombine)
+	{
 		Stream<BinaryInput> inps = Arrays.stream(toCombine).map(it -> it.input);
-		this.input = BinaryInput.combineAND(Stream.concat(inps, Stream.of(input)).toArray(BinaryInput[]::new));
-		return this;
+		BinaryInput newInput = BinaryInput
+				.combineAND(Stream.concat(inps, Stream.of(input)).toArray(BinaryInput[]::new));
+		return new DigitalIn(newInput);
 	}
 
 	/**
-	 * combines a list of boolean streams into a single stream that returns true if any of the
-	 * original streams are true
+	 * combines a list of boolean streams into a single stream that returns true
+	 * if any of the original streams are true
 	 * 
-	 * @param toCombine the list of streams to combine
+	 * @param toCombine
+	 *                      the list of streams to combine
 	 * @return the combined stream
 	 */
-	public DigitalIn combineOR(DigitalIn... toCombine) {
+	public DigitalIn getCombineOR(DigitalIn... toCombine)
+	{
 		Stream<BinaryInput> inps = Arrays.stream(toCombine).map(it -> it.input);
-		this.input = BinaryInput.combineOR(Stream.concat(inps, Stream.of(input)).toArray(BinaryInput[]::new));
-		return this;
+		BinaryInput newInput = BinaryInput.combineOR(Stream.concat(inps, Stream.of(input)).toArray(BinaryInput[]::new));
+		return new DigitalIn(newInput);
 	}
 
 	/**
 	 * @return the inverted boolean stream
 	 */
-	public DigitalIn invert() {
-		this.input = BinaryInput.invert(input);
-		return this;
+	public DigitalIn getInverted()
+	{
+		BinaryInput newInput = BinaryInput.invert(input);
+		return new DigitalIn(newInput);
 	}
 
 	/**
-	 * generates a new stream that passes the output values through the given unary operation to the
-	 * original stream
+	 * generates a new stream that passes the output values through the given
+	 * unary operation to the original stream
 	 * 
-	 * @param operation the unary operation (takes a boolean value returns a mapped value)
+	 * @param operation
+	 *                      the unary operation (takes a boolean value returns a
+	 *                      mapped value)
 	 * @return the mapped stream
 	 */
-	public DigitalIn map(UnaryOperator<Boolean> operation) {
-		this.input = BinaryInput.map(input, operation);
-		return this;
+	public DigitalIn map(UnaryOperator<Boolean> operation)
+	{
+		BinaryInput newInput = BinaryInput.map(input, operation);
+		return new DigitalIn(newInput);
 	}
 
-	public DigitalIn addChangeListener(Consumer<Boolean> onChange, boolean asyncPoll) {
+	public DigitalIn addChangeListener(Consumer<Boolean> onChange, boolean asyncPoll)
+	{
 		ListeningBinaryInput listeningSource = BinaryInput.getListeningSource(input, onChange);
-		input = listeningSource;
-		if (asyncPoll) {
+		if (asyncPoll)
+		{
 			ListenerLooper.addListener(listeningSource);
 		}
 
-		return this;
+		return new DigitalIn(listeningSource);
 	}
 
-	public DigitalIn copy() {
+	public DigitalIn copy()
+	{
 		return new DigitalIn(input);
 	}
 
 	/**
 	 * @return the value of the boolean stream
 	 */
-	public boolean get() {
+	public boolean get()
+	{
 		return input.get();
 	}
 }

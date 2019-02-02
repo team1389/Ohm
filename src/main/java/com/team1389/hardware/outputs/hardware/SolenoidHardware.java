a4af63860630285829c2ddf5e71fbb5993e12028
Quunii <kennethmiuraubw@gmail.com>
2019-02-02 15:59:53 -0500

    module num is now CAN port

--- src/main/java/com/team1389/hardware/outputs/hardware/SolenoidHardware.java
+++ src/main/java/com/team1389/hardware/outputs/hardware/SolenoidHardware.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import com.team1389.hardware.Hardware;
 import com.team1389.hardware.outputs.software.DigitalOut;
 import com.team1389.hardware.registry.Registry;
+import com.team1389.hardware.registry.port_types.CAN;
 import com.team1389.hardware.registry.port_types.PCM;
 import com.team1389.util.list.AddList;
 import com.team1389.watch.Watchable;
@@ -17,52 +18,68 @@ import edu.wpi.first.wpilibj.Solenoid;
  * @author amind
  *
  */
-public class SolenoidHardware extends Hardware<PCM> {
-	private int moduleNum;
+public class SolenoidHardware extends Hardware<PCM>
+{
+	private CAN moduleID;
 
 	/**
-	 * @param requestedPort the port to attempt to initialize this hardware
-	 * @param registry the registry associated with the robot
+	 * @param requestedPort
+	 *                          the port to attempt to initialize this hardware
+	 * @param registry
+	 *                          the registry associated with the robot
 	 */
-	public SolenoidHardware(PCM requestedPort, Registry registry) {
-		this(1, requestedPort, registry);
+	public SolenoidHardware(PCM requestedPort, Registry registry)
+	{
+		this(new CAN(1), requestedPort, registry);
 	}
 
 	/**
-	 * @param requestedPort the port to attempt to initialize this hardware
-	 * @param registry the registry associated with the robot
+	 * @param moduleID
+	 *                          the port of the PCM that this solenoid is
+	 *                          connected to
+	 * @param requestedPort
+	 *                          the port to attempt to initialize this hardware
+	 * @param registry
+	 *                          the registry associated with the robot
 	 */
-	public SolenoidHardware(int moduleNum, PCM requestedPort, Registry registry) {
-		this.moduleNum = moduleNum;
+	public SolenoidHardware(CAN moduleID, PCM requestedPort, Registry registry)
+	{
+		this.moduleID = moduleID;
 	}
 
 	private Optional<Solenoid> wpiSolenoid;
 
 	/**
 	 * 
-	 * @return a boolean output stream that controls the position of the solenoid
+	 * @return a boolean output stream that controls the position of the
+	 *         solenoid
 	 */
-	public DigitalOut getDigitalOut() {
+	public DigitalOut getDigitalOut()
+	{
 		return new DigitalOut(val -> wpiSolenoid.ifPresent(w -> w.set(val)));
 	}
 
 	@Override
-	public AddList<Watchable> getSubWatchables(AddList<Watchable> stem) {
+	public AddList<Watchable> getSubWatchables(AddList<Watchable> stem)
+	{
 		return super.getSubWatchables(stem).put(getDigitalOut().getWatchable("state"));
 	}
 
 	@Override
-	protected String getHardwareIdentifier() {
+	protected String getHardwareIdentifier()
+	{
 		return "Solenoid";
 	}
 
 	@Override
-	public void init(PCM port) {
-		wpiSolenoid = Optional.of(new Solenoid(moduleNum, port.index()));
+	public void init(PCM port)
+	{
+		wpiSolenoid = Optional.of(new Solenoid(moduleID.index(), port.index()));
 	}
 
 	@Override
-	public void failInit() {
+	public void failInit()
+	{
 		wpiSolenoid = Optional.empty();
 	}
 

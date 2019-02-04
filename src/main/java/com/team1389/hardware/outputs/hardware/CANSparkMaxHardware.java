90f9e81f8d2412511e29dcbbbadec250680091ae
Raphael Metz <raphaeljmetz@gmail.com>
2019-02-04 16:09:12 -0500

    Deleted Configuration from SparkMAXHardware

--- src/main/java/com/team1389/hardware/outputs/hardware/CANSparkMaxHardware.java
+++ src/main/java/com/team1389/hardware/outputs/hardware/CANSparkMaxHardware.java
@@ -13,72 +13,84 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 /**
- * Provides input (position) or output (voltage control) streams for the SparkMax controlled by CAN 
+ * Provides input (position) or output (voltage control) streams for the
+ * SparkMax controlled by CAN
  */
-public class CANSparkMaxHardware extends Hardware<CAN> {
-	
+public class CANSparkMaxHardware extends Hardware<CAN>
+{
+
 	public static final int kTimeoutMs = 10;
 
 	private boolean outInverted;
 	private boolean inInverted;
 	private CAN requestedPort;
 	private Optional<CANSparkMax> revSpark;
-	private boolean brushless;
 	private boolean idleModeCoast;
 
-
-	public CANSparkMaxHardware(Configuration config, CAN requestedPort, Registry registry){
-		outInverted = config.isOutputInverted();
-		inInverted = config.isInputInverted();
-		brushless = config.isBrushless();
-		idleModeCoast = config.isCoastInIdleMode();
+	public CANSparkMaxHardware(boolean outInverted, boolean inInverted, boolean idleModeCoast, CAN requestedPort,
+			Registry registry)
+	{
+		this.outInverted = outInverted;
+		this.inInverted = inInverted;
+		this.idleModeCoast = idleModeCoast;
+		this.requestedPort = requestedPort;
 		attachHardware(requestedPort, registry);
 	}
-	
-	public PercentOut getVoltageController(){
+
+	public PercentOut getVoltageController()
+	{
 		return new PercentOut(voltage -> revSpark.ifPresent(spark -> spark.set(voltage)));
 	}
-	
+
 	/**
 	 * 
-	 * @param limit Current in amps to keep spark current draw below
+	 * @param limit
+	 *                  Current in amps to keep spark current draw below
 	 */
-	public void setCurrentLimit(int limit) {
+	public void setCurrentLimit(int limit)
+	{
 		revSpark.ifPresent(spark -> spark.setSmartCurrentLimit(limit));
 	}
-	
-	/** Precondition: 
-	 * Will not read encoder values properly unless Spark is properly configured for whichever sensor is being used
-	 * Requires encoder to be plugged in
+
+	/**
+	 * Precondition: Will not read encoder values properly unless Spark is
+	 * properly configured for whichever sensor is being used Requires encoder
+	 * to be plugged in
+	 * 
 	 * @return Stream which provides position in rotations
 	 * 
 	 */
-	//TODO: Figure out how to configure spark from code end instead of manually via the client
-	public RangeIn<Position> getPositionStreamInRotations(){
-		// Min/Max in RangeIn don't matter. That's why the max is set to one. Doesn't cap off values
-		if(inInverted){
-			return new RangeIn<>(Position.class, () -> revSpark.map(spark -> -spark.getEncoder().getPosition()).orElse(0.0), 0, 1);
+	// TODO: Figure out how to configure spark from code end instead of manually
+	// via the client
+	public RangeIn<Position> getPositionStreamInRotations()
+	{
+		// Min/Max in RangeIn don't matter. That's why the max is set to one.
+		// Doesn't cap off values
+		if (inInverted)
+		{
+			return new RangeIn<>(Position.class,
+					() -> revSpark.map(spark -> -spark.getEncoder().getPosition()).orElse(0.0), 0, 1);
 		}
-		return new RangeIn<>(Position.class, () -> revSpark.map(spark -> spark.getEncoder().getPosition()).orElse(0.0), 0, 1);
+		return new RangeIn<>(Position.class, () -> revSpark.map(spark -> spark.getEncoder().getPosition()).orElse(0.0),
+				0, 1);
 	}
-	
+
 	/**
 	 * runs if requested port is open
 	 */
-    @Override
-	protected void init(CAN port) {
+	@Override
+	protected void init(CAN port)
+	{
 		CANSparkMax spark;
-		if(brushless) {
-			spark = new CANSparkMax(requestedPort.index(), MotorType.kBrushless);
-		} else {
-			spark = new CANSparkMax(requestedPort.index(), MotorType.kBrushed);
-		}
+		spark = new CANSparkMax(requestedPort.index(), MotorType.kBrushless);
 		spark.setCANTimeout(kTimeoutMs);
 		spark.setInverted(outInverted);
-		if(idleModeCoast){
+		if (idleModeCoast)
+		{
 			spark.setIdleMode(IdleMode.kCoast);
 		}
-		else{
+		else
+		{
 			spark.setIdleMode(IdleMode.kBrake);
 		}
 		revSpark = Optional.of(spark);
@@ -88,60 +100,21 @@ public class CANSparkMaxHardware extends Hardware<CAN> {
 	 * runs if requested port is taken
 	 */
 	@Override
-	protected void failInit() {
+	protected void failInit()
+	{
 		revSpark = Optional.empty();
 	}
 
 	@Override
-	protected String getHardwareIdentifier() {
+	protected String getHardwareIdentifier()
+	{
 		return "Spark MAX";
 	}
-	
-	/** Stores all configuration options for SparkMax. Expects to be passed in to constructor of CANSparkMaxHardware
+
+	/**
+	 * Stores all configuration options for SparkMax. Expects to be passed in to
+	 * constructor of CANSparkMaxHardware
 	 * 
 	 */
-	public class Configuration{
-		private boolean outputInverted = false;
-		private boolean inputInverted = false;
-		private boolean brushless = true;
-		private boolean idleModeCoast = true;
-		/**
-		 * @param brushless if Motor being controlled is brushless
-		 */
-		public void setToBrushless(boolean brushless) {
-			this.brushless = brushless;
-		}
-		/**
-		 * @param idleModeCoast if motor should be coast in idle mode
-		 */
-		public void setToCoastInIdleMode(boolean idleModeCoast) {
-			this.idleModeCoast = idleModeCoast;
-		}
-		/**
-		 * @param inInverted if sensor input should be inverted
-		 */
-		public void setToInputInverted(boolean inputInverted) {
-			this.inputInverted = inputInverted;
-		}
-		/**
-		 * @param outInverted if voltage output should be inverted
-		 */
-		public void setToOutputInverted(boolean outputInverted) {
-			this.outputInverted = outputInverted;
-		}
-		public boolean isBrushless(){
-			return brushless;
-		} 
-		public boolean isCoastInIdleMode(){
-			return idleModeCoast;
-		}
-		public boolean isInputInverted(){
-			return inputInverted;
-		}
-		public boolean isOutputInverted(){
-			return outputInverted;
-		}
-	}
-
 
 }
